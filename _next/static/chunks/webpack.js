4d7d83f20216779ca13edf460ea21254f203b05f
smly <smly@users.noreply.github.com>
2020-11-09 04:50:19 +0900

    Add card images

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "502de98456ec6614f0e6";
+/******/ 	var hotCurrentHash = "dacf5c58f6b7e1a6251e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
